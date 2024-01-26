c951c627a8e763a8dbd28a1ea5a57ff8d7c1b5ba
krittamark <33484828+krittamark@users.noreply.github.com>
2024-01-26 14:12:05 +0700

    chore: change indent spacing

--- Week3/SimpleMath.java
+++ Week3/SimpleMath.java
@@ -1,11 +1,12 @@
 public class SimpleMath {
-	public static double PI = 3.14159;
-	public static double e =  2.71828;
-	public int add(int a, int b) {
-		return a + b;
-	}
-	public int subtract(int a, int b) {
-		return a - b;
-	}
-}
+  public static double PI = 3.14159;
+  public static double e = 2.71828;
 
+  public int add(int a, int b) {
+    return a + b;
+  }
+
+  public int subtract(int a, int b) {
+    return a - b;
+  }
+}
