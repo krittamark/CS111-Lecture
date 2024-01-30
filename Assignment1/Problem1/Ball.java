8b7969f59c152a05e89b1209728c1981e467ea2f
krittamark <33484828+krittamark@users.noreply.github.com>
2024-01-31 02:08:28 +0700

    feat: add assignment 1 problem 1's solution

---
+++ Assignment1/Problem1/Ball.java
@@ -0,0 +1,41 @@
+// Author: Krittamet Damthongkam (กฤตเมธ ดำทองคำ)
+// Student ID: 6609490062
+
+public class Ball {
+  /**
+   * The radius of the ball.
+   */
+  private double radius;
+
+  /**
+   * Retrieve the radius of the ball.
+   * @return The radius of the ball.
+   */
+  public double getRadius() {
+    return radius;
+  }
+
+  /**
+   * Set the radius of the ball.
+   * @param aRadius The radius of the ball.
+   */
+  public void setRadius(double aRadius) {
+    this.radius = aRadius;
+  }
+
+  /**
+   * Retrieve the surface area of the ball.
+   * @return The surface area of the ball.
+   */
+  public double getSurface() {
+    return 4.0 * Math.PI * Math.pow(radius, 2);
+  }
+
+  /**
+   * Retrieve the volume of the ball.
+   * @return The volume of the ball.
+   */
+  public double getVolume() {
+    return (4.0 / 3.0) * Math.PI * Math.pow(radius, 3);
+  }
+}
---
+++ Assignment1/Problem1/BallTest.java
@@ -0,0 +1,24 @@
+// Author: Krittamet Damthongkam (กฤตเมธ ดำทองคำ)
+// Student ID: 6609490062
+
+import java.util.Scanner;
+
+public class BallTest {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.print("Input the radius of the ball > ");
+    double radius = scanner.nextDouble();
+
+    Ball ball = new Ball();
+    ball.setRadius(radius);
+
+    double surfaceArea = ball.getSurface();
+    double volume = ball.getVolume();
+
+    System.out.printf("The ball radius '%.2f' has surface area = %.3f%n", radius, surfaceArea);
+    System.out.printf("The ball radius '%.2f' has volume = %.3f%n", radius, volume);
+
+    scanner.close();
+  }
+}
